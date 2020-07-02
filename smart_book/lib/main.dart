38fe47f1bfef6e8e7d3d5289510f934c8017024f
ReginaExMachina <we.love.rachel.day.lots@gmail.com>
2020-07-02 01:22:26 -0400

    Add route

--- smart_book/lib/main.dart
+++ smart_book/lib/main.dart
@@ -62,7 +62,33 @@ class _ServiceSuggestionsState extends State<ServiceSuggestions> {
         }
 
         void _pushSaved() {
-          
+            Navigator.of(context).push(
+              MaterialPageRoute<void>(
+                builder: (BuildContext context) {
+                  final tiles = _saved.map(
+                    (WordPair pair) {
+                      return ListTile(
+                        title: Text(
+                          pair.asPascalCase,
+                          style: _biggerFont,
+                        ),
+                      );
+                    },
+                  );
+                  final divided = ListTile.divideTiles(
+                    context: context,
+                    tiles: tiles,
+                  ).toList();
+
+                  return Scaffold(
+                    appBar: AppBar(
+                      title: Text('Saved Suggestions'),
+                    ),
+                    body: ListView(children: divided),
+                  );
+                }
+              )
+            );
         }
         
             @override
