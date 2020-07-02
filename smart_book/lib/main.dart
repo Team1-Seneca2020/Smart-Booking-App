13f16c765f231b42191cfce67270a7fc8a6177e4
ReginaExMachina <we.love.rachel.day.lots@gmail.com>
2020-07-02 15:41:38 -0400

    End of experiment

--- smart_book/lib/main.dart
+++ smart_book/lib/main.dart
@@ -8,18 +8,135 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'BookSmart',
-      home: ServiceSuggestions(),      
+      home: HomePage(),
+      theme: ThemeData(
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.white,
+
+        fontFamily: 'Arial',
+      )      
     );
   }
 }
 
+
+//******** CLASSES ********/ 
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class HomeContent extends StatefulWidget {
+  @override
+  _HomeContentState createState() => _HomeContentState();
+}
 
 class ServiceSuggestions extends StatefulWidget {
   @override
   _ServiceSuggestionsState createState() => _ServiceSuggestionsState();
 }
 
-class _ServiceSuggestionsState extends State<ServiceSuggestions> {
+//******** STATES ********/ 
+class _HomePageState extends State<HomePage> {
+  void _settings() {
+    // TBU
+  }
+            @override
+            Widget build(BuildContext context) {
+              return Scaffold(
+                appBar: AppBar(
+                  title: Text('BookSmart'),
+                  actions: [
+                    IconButton(icon: Icon(Icons.list), onPressed: _settings)
+                  ],
+                ),
+                body: Center(
+                  child: HomeContent()
+                ),
+                bottomNavigationBar: BottomNavigationBar(
+                  items: <BottomNavigationBarItem>[
+                    BottomNavigationBarItem(icon: Icon(Icons.home), title: Text('Main')),
+                    BottomNavigationBarItem(icon: Icon(Icons.chat), title: Text('Chat')),
+                    BottomNavigationBarItem(icon: Icon(Icons.perm_contact_calendar), title: Text('Bookings')),
+                ]
+                ),
+              );
+            }  
+}
+
+class _HomeContentState extends State<HomeContent> {
+  void _search() {
+    //TBU
+  }
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Search'),
+        actions: [IconButton(icon: Icon(Icons.search), onPressed: _search)]),
+        body: Center(
+          child: ServiceSuggestions(),
+          ),
+      );
+    }
+
+}
+
+  class ServiceListItem extends StatelessWidget {
+    const ServiceListItem({
+      this.thumbnail,
+      this.title,
+    });
+
+    final Widget thumbnail;
+    final String title;
+
+    @override
+    Widget build(BuildContext context) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Expanded(
+              flex: 2,
+              child: thumbnail,
+            ),
+            Expanded(
+              flex: 3,
+              child: Text(title),
+            ),
+          ],
+        ),
+      );
+    }
+    
+  }
+
+class _ServiceSuggestionsState extends State<ServiceSuggestions> {  
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.all(0.0),
+      itemExtent: 106.0,
+      children: <ServiceListItem> [
+        ServiceListItem(
+          thumbnail: Container(
+            decoration: const BoxDecoration(color: Colors.blue),
+          ),
+          title: 'Cafe'
+        ),
+      ],
+    );
+  }
+}
+
+// From tutorial
+class ListExample extends StatefulWidget {
+  @override
+  _ListExampleState createState() => _ListExampleState();
+}
+
+class _ListExampleState extends State<ListExample> {
     final _suggestions = <WordPair>[];
     final _saved = Set<WordPair>();
     final _biggerFont = TextStyle(fontSize: 18.0);
